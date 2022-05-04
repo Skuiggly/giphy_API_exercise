c3dadd431a60bd5b89969a1f94038d6bdc2895c6
Skuiggly <piplup107@gmail.com>
2022-05-04 15:35:23 +0200

    Add error handling for getGiphy

--- giphy.js
+++ giphy.js
@@ -10,7 +10,12 @@ function getElValue(el){
     return el.value;
 }
 async function getGiphy(query, apikey="lqxd1xSb2SU6PBWDI0hezzCOyIqF1bRA") {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apikey}&s=${query}`, {mode: "cors"});
-    const catData = await response.json();
-    imgEl.src = catData.data.images.original.url;
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apikey}&s=${query}`, {mode: "cors"});
+        const catData = await response.json();
+        imgEl.src = catData.data.images.original.url;
+    
+    } catch (error) {
+        console.error(error);
+    }
 }
