c0f09d5653968031715f3b08b47c13d6472a9022
Skuiggly <piplup107@gmail.com>
2022-05-04 15:22:10 +0200

    Refactor getGiphy() with async/await instead of fetch

--- giphy.js
+++ giphy.js
@@ -9,15 +9,8 @@ function getElValue(el){
     console.log(el.value);
     return el.value;
 }
-function getGiphy(query, apikey="lqxd1xSb2SU6PBWDI0hezzCOyIqF1bRA") {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apikey}&s=${query}`, {mode: "cors"})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            imgEl.src = response.data.images.original.url;
-        })
-        .catch(function(err) {
-            console.error(err);
-        });
+async function getGiphy(query, apikey="lqxd1xSb2SU6PBWDI0hezzCOyIqF1bRA") {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apikey}&s=${query}`, {mode: "cors"});
+    const catData = await response.json();
+    imgEl.src = catData.data.images.original.url;
 }
